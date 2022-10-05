d6c145aeadc8dbe11e11709652f884a21ed5b1fd
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2022-10-05 02:07:37 -0700

    Completed assignment

---
+++ adventure.js
@@ -0,0 +1,31 @@
+let place = prompt("Do you want to go to the 'park' or the 'mall?'")
+
+if(place === 'mall'){
+    alert('you are going to the mall')
+    let activity = prompt("do you want to go to the 'zoo' or to the 'amusement park?'")
+    if(activity === 'amusement park'){   
+     alert('you are going to the amusement park')
+    
+    }
+    else if(activity === 'zoo'){
+        alert("you are going to the zoo")
+
+    }
+    }
+
+     else if(place === 'park'){
+        alert('you are going to the park')
+    }
+
+let choice = prompt("do you want to go to ride 'haunted mansion' or pet the 'elephants'?type elephants or haunted mansion")
+
+if(choice === 'haunted mansion'){
+    alert('you are going to haunted mansion! boo!! spooky! The end.')
+    }
+
+    else if(choice === 'elephants'){
+        alert('you are going to pet the elephnats have a great day! The end.')
+    }
+
+
+
